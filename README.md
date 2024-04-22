c306966c7ee28c9aafd2bedc1d38b787c5c90447
misostack <misostack.com@gmail.com>
2024-04-22 16:36:52 +0700

    add styles, processed assets, unprocessed assets

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 ## Getting start
 
 ## References
+
+- [TailwindCSS Preflight](https://tailwindcss.com/docs/preflight#overview)
+
+## Dev Diary
+
+### Day 001 : Setup Structure
+
+> Stylesheets
+
+- We need to add tailwindcss into our project as a main css framework.
+
+```sh
+npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
+npx tailwindcss init -p
+```
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/s-logo.png" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <a href="https://vuejs.org/" target="_blank"> </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
